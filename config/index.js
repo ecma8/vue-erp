a0271e27b9d88861eb1b64fa95c0630efb5676de
ecma8 <756932563@qq.com>
2018-05-02 16:42:00 +0800

    change webpack config dev port and set autoOpenBrowser

--- config/index.js
+++ config/index.js
@@ -15,8 +15,8 @@ module.exports = {
     },
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    port: 6000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: true,
     errorOverlay: true,
 
     notifyOnErrors: true,
